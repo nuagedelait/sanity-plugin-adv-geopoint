93056b95f2fe0c23b3f24408a51ae198323e59db
nuagedelait <oliver@nuagedelait.com>
2023-02-20 22:54:05 +0100

    release/0.0.1: add configurations

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: false
+  },
+  extends: [
+    'sanity',
+    'sanity/typescript',
+    'sanity/react',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
+  ],
+  overrides: [
+    {
+      files: ['*.{ts,tsx}']
+    }
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    project: './tsconfig.json'
+  },
+  plugins: ['prettier'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/no-shadow': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_'
+      }
+    ],
+    'no-shadow': 'off',
+    'react/display-name': 0,
+    'react/jsx-no-bind': 0,
+    'no-use-before-define': 0
+  },
+  settings: {
+    'import/ignore': ['.*node_modules.*', '.*:.*'],
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  }
+}
